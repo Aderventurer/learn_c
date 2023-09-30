67a7586095348e66d1bbdd29bb05cac6bb11dd19
Adventurer <2055976331@qq.com>
2023-09-30 10:38:59 +0800

    define a new function

--- prime_number.c
+++ prime_number.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
-int main(){
-    int num;
-    printf("enter a num:\n");
-    scanf("%d",&num);
+
+// to check if N is prim number or not
+void checkprime(int N){
     int flag = 1;
-    for(int i = 2 ; i <= num/2 ; i++){
-        if(num % i == 0){
+    for(int i = 2 ; i <= N/2 ; i++){
+        if(N % i == 0){
             flag = 0;
             break;
         }
     }
     if (flag){
-        printf("the num is a prime number");
+        printf("the num %d is a prime number",N);
     }
     else{
-        printf("the number is not a prime number");
+        printf("the number %d is not a prime number",N);
     }
-        
+}
+
+int main(){
+    int num;
+    printf("enter a num:\n");
+    scanf("%d",&num);
+    checkprime(num);
     return 0;
-
-
 }
