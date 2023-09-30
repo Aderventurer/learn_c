54be8f9bee795a7c6898a2bf0932e750699845b6
Adventurer <2055976331@qq.com>
2023-09-30 10:57:22 +0800

    use while instead of for

--- prime_number.c
+++ prime_number.c
@@ -3,12 +3,17 @@
 // to check if N is prim number or not
 void checkprime(int N){
     int flag = 1;
-    for(int i = 2 ; i <= N/2 ; i++){
-        if(N % i == 0){
+    int i = 2;
+    while(i <= N/2)
+    {   
+        if (N % i == 0)
+        {
             flag = 0;
             break;
         }
+        i++;
     }
+      
     if (flag){
         printf("the num %d is a prime number",N);
     }
