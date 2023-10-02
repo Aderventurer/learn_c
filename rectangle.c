0494ab8a8bf4d4d30b024b782181223245e18314
Adventurer <2055976331@qq.com>
2023-10-02 20:27:49 +0800

    rectangle

---
+++ rectangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    int l = 10, b = 10;
+    printf("Area of rectangle is : %d", l * b);
+    printf("\nPerimeter of rectangle is : %d", 2 * (l + b));
+    return 0;
+}
