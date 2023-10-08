92f559a6c9284500707f3cbd85122bc08e756921
Adventurer <2055976331@qq.com>
2023-10-08 22:48:18 +0800

    reserve_an_integer

---
+++ reserve_an_integer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int reverseDigits(int num) 
+{ 
+    int rev_num = 0; 
+    while (num > 0) { 
+        rev_num = rev_num * 10 + num % 10; 
+        num = num / 10; 
+    } 
+    return rev_num; 
+}  
+int main() 
+{ 
+    int num = 4562; 
+    printf("Reverse of  is %d", reverseDigits(num)); 
+  
+    getchar(); 
+    return 0; 
+}
