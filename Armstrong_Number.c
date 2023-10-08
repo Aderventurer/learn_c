37844c7ce56d8fb78e3f5680c0a72f1e1404f115
Aderventurer <143814111+Aderventurer@users.noreply.github.com>
2023-10-08 22:41:15 +0800

    Update Armstrong_Number.c

--- Armstrong_Number.c
+++ Armstrong_Number.c
@@ -6,8 +6,6 @@ int main()
     int i, sum, num, count = 0;
     printf(
         "All Armstrong number between 1 and 1000 are:\n");
- 
-    // This loop will run for 1 to 1000
     for (i = 1; i <= 1000; i++) {
         num = i;
         // Count number of digits.
